d3dd6c5ef1a117785a66d0a95da15675e67bd233
aleksey-ka <aleksey_ka@abbyy.com>
2020-10-28 14:19:51 +0300

    Center main frame on the screen

--- main.cpp
+++ main.cpp
@@ -1,10 +1,19 @@
-#include "mainframe.h"
 #include <QApplication>
+#include <QDesktopWidget>
 
-int main(int argc, char *argv[])
+#include "mainframe.h"
+
+int main( int argc, char *argv[] )
 {
-    QApplication a(argc, argv);
+    QApplication a( argc, argv );
     MainFrame w;
+
+    // Center main frame on the screen
+    QRect screenGeometry = QApplication::desktop()->screenGeometry();
+    int x = ( screenGeometry.width() - w.width() ) / 2;
+    int y = ( screenGeometry.height() - w.height() ) / 2;
+    w.move( x, y );
+
     w.show();
 
     return a.exec();
