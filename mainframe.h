523a5fe88119aad6bceb1260605bf89d36299620
Aleksey Kalyuzhny <aleksey_ka@abbyy.com>
2020-11-11 00:00:02 +0300

    Added copyright headers

--- mainframe.h
+++ mainframe.h
@@ -1,3 +1,6 @@
+// Copyright (C) 2020 Aleksey Kalyuzhny. Released under the terms of the
+// GNU General Public License version 3. See <http://www.gnu.org/licenses/>
+
 #ifndef MAINFRAME_H
 #define MAINFRAME_H
 
