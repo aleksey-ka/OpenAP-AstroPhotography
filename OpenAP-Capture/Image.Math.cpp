e88722c6aee67fe206c2ba6aa522d3a115798557
Aleksey Kalyuzhny <aleksey_ka@abbyy.com>
2021-09-03 13:26:01 +0300

    Refactoring pixel math. New histogram statistics

--- OpenAP-Capture/Image.Math.cpp
+++ OpenAP-Capture/Image.Math.cpp
@@ -2,3 +2,32 @@
 // GNU General Public License version 3. See <http://www.gnu.org/licenses/>
 
 #include "Image.Math.h"
+
+CHistogram::CHistogram( size_t numberOfChannels, size_t bitsPerChannel ) :
+    channelSize( 2 << bitsPerChannel )
+{
+    channels.resize( numberOfChannels );
+    for( size_t i = 0; i < numberOfChannels; i++ ) {
+        channels[i].resize( channelSize );
+    }
+}
+
+size_t pixels_histogram_p( const CHistogram& h, size_t channel, size_t target )
+{
+    auto& ch = h[channel];
+    size_t channelSize = h.ChannelSize();
+    size_t sum = 0;
+    for( size_t i = 0; i < channelSize; i++ ) {
+        size_t delta = target - sum;
+        size_t v = ch[i];
+        if( delta <= v ) {
+            if( i > 0 && delta <= v / 2 ) {
+                return i - 1;
+            } else {
+                return i;
+            }
+        }
+        sum += v;
+    }
+    return INT_MAX;
+}
--- OpenAP-Capture/Image.Math.h
+++ OpenAP-Capture/Image.Math.h
@@ -9,9 +9,98 @@
 #include <map>
 #include <cmath>
 #include <limits>
+#include <cassert>
+
+template<typename T1, typename T2>
+inline void pixels_set( T1* dst, const T2* src, size_t count, size_t numberOfChannels = 1 )
+{
+    for( size_t i = 0; i < count * numberOfChannels; i++ ) {
+        dst[i] = src[i];
+    }
+}
+
+template<typename T1, typename T2>
+inline void pixels_set_value( T1* dst, T2 value, size_t count, size_t numberOfChannels = 1 )
+{
+    for( size_t i = 0; i < count * numberOfChannels; i++ ) {
+        dst[i] = value;
+    }
+}
+
+template<typename T1, typename T2>
+inline void pixels_add( T1* dst, const T2* src, size_t count, size_t numberOfChannels = 1 )
+{
+    for( size_t i = 0; i < count * numberOfChannels; i++ ) {
+        dst[i] += src[i];
+    }
+}
+
+template<typename T1, typename T2>
+inline void pixels_add_value( T1* dst, T2 value, size_t count, size_t numberOfChannels = 1 )
+{
+    for( size_t i = 0; i < count * numberOfChannels; i++ ) {
+        dst[i] += value;
+    }
+}
+
+template<typename T1, typename T2>
+inline void pixels_subtract( T1* dst, const T2* src, size_t count, size_t numberOfChannels = 1 )
+{
+    for( size_t i = 0; i < count * numberOfChannels; i++ ) {
+        dst[i] -= src[i];
+    }
+}
+
+template<typename T1, typename T2>
+inline void pixels_subtract_value( T1* dst, T2 value, size_t count, size_t numberOfChannels = 1 )
+{
+    for( size_t i = 0; i < count * numberOfChannels; i++ ) {
+        dst[i] -= value;
+    }
+}
+
+template<typename T1, typename T2, typename T3>
+inline void pixels_multiply( T1* dst, const T2* src, T3 value, size_t count, size_t numberOfChannels = 1 )
+{
+    for( size_t i = 0; i < count * numberOfChannels; i++ ) {
+        dst[i] = src[i] * value;
+    }
+}
+
+template<typename T1, typename T2>
+inline void pixels_multiply_by_value( T1* dst, T2 value, size_t count, size_t numberOfChannels = 1 )
+{
+    for( size_t i = 0; i < count * numberOfChannels; i++ ) {
+        dst[i] *= value;
+    }
+}
+
+template<typename T1, typename T2, typename T3>
+inline void pixels_divide( T1* dst, const T2* src, T3 value, size_t count, size_t numberOfChannels = 1 )
+{
+    for( size_t i = 0; i < count * numberOfChannels; i++ ) {
+        dst[i] = src[i] / value;
+    }
+}
+
+template<typename T1, typename T2>
+inline void pixels_divide_by_value( T1* dst, T2 value, size_t count, size_t numberOfChannels = 1 )
+{
+    for( size_t i = 0; i < count * numberOfChannels; i++ ) {
+        dst[i] /= value;
+    }
+}
+
+template<typename T1, typename T2, typename T3>
+inline void pixels_divide_round( T1* dst, const T2* src, T3 value, size_t count, size_t numberOfChannels = 1 )
+{
+    for( size_t i = 0; i < count * numberOfChannels; i++ ) {
+        dst[i] = std::round( src[i] / value );
+    }
+}
 
 template<class T, int numberOfChannels = 1>
-std::tuple<double, double, T, T> simple_pixel_statistics( const T* pixels, size_t count, int channel = 0 )
+std::tuple<double, double, T, T> simple_pixel_statistics( const T* pixels, size_t count, size_t channel = 0 )
 {
     auto minValue = std::numeric_limits<T>::max();
     auto maxValue = std::numeric_limits<T>::min();
@@ -33,68 +122,59 @@ std::tuple<double, double, T, T> simple_pixel_statistics( const T* pixels, size_
     return std::make_tuple( sum_v / count, sqrt( sum_vv / count - ( sum_v * sum_v ) / count / count ), minValue, maxValue );
 }
 
-template<typename T1, typename T2>
-void pixels_set_value( T1* dst, T2 value, size_t count, int numberOfChannels = 1 )
+class CHistogram {
+public:
+    CHistogram( size_t numberOfChannels, size_t bitsPerChannel );
+
+    std::vector<unsigned int>& operator [] ( size_t index ) { return channels[index]; }
+    const std::vector<unsigned int>& operator [] ( size_t index ) const { return channels[index]; }
+    void SetPixelsCount( unsigned int value ) { count = value; }
+
+    unsigned int ChannelSize() const { return channelSize; }
+    unsigned int PixelsCount() const { return count; }
+
+private:
+    const unsigned int channelSize;
+    std::vector<std::vector<unsigned int>> channels;
+    unsigned int count = 0;
+};
+
+template<typename T>
+inline CHistogram pixels_histogram( const T* pixels, size_t count, size_t bitsPerChannel, size_t numberOfChannels = 1 )
 {
-    for( size_t i = 0; i < count * numberOfChannels; i++ ) {
-        dst[i] = value;
+    if( numberOfChannels == 1 ) {
+        // Monochrome
+        CHistogram result( 1, bitsPerChannel );
+        auto& ch0 = result[0];
+        for( size_t i = 0; i < count; i++ ) {
+            ch0[pixels[i]]++;
+        }
+        result.SetPixelsCount( count );
+        return result;
+    } else if( numberOfChannels == 3 ) {
+        // Color
+        CHistogram result( 3, bitsPerChannel );
+        auto& ch0 = result[0];
+        auto& ch1 = result[1];
+        auto& ch2 = result[2];
+        for( size_t i = 0; i < count; i++ ) {
+            const T* p = pixels + 3 * i;
+            ch0[p[0]]++;
+            ch1[p[1]]++;
+            ch2[p[2]]++;
+        }
+        result.SetPixelsCount( count );
+        return result;
+    } else {
+        assert( false );
     }
 }
 
-template<typename T1, typename T2>
-void pixels_copy( T1* dst, const T2* src, size_t count, int numberOfChannels = 1 )
-{
-    for( size_t i = 0; i < count * numberOfChannels; i++ ) {
-        dst[i] = src[i];
-    }
-}
+size_t pixels_histogram_p( const CHistogram& h, size_t channel, size_t target );
 
-template<typename T1, typename T2>
-void pixels_add( T1* dst, const T2* src, size_t count, int numberOfChannels = 1 )
+inline size_t pixels_histogram_median( const CHistogram& h, size_t channel )
 {
-    for( size_t i = 0; i < count * numberOfChannels; i++ ) {
-        dst[i] += src[i];
-    }
-}
-
-template<typename T1, typename T2>
-void pixels_add_value( T1* dst, T2 value, size_t count, int numberOfChannels = 1 )
-{
-    for( size_t i = 0; i < count * numberOfChannels; i++ ) {
-        dst[i] += value;
-    }
-}
-
-template<typename T1, typename T2>
-void pixels_subtract( T1* dst, const T2* src, size_t count, int numberOfChannels = 1 )
-{
-    for( size_t i = 0; i < count * numberOfChannels; i++ ) {
-        dst[i] -= src[i];
-    }
-}
-
-template<typename T1, typename T2>
-void pixels_subtract_value( T1* dst, T2 value, size_t count, int numberOfChannels = 1 )
-{
-    for( size_t i = 0; i < count * numberOfChannels; i++ ) {
-        dst[i] -= value;
-    }
-}
-
-template<typename T1, typename T2>
-void pixels_divide( T1* dst, T2 value, size_t count, int numberOfChannels = 1 )
-{
-    for( size_t i = 0; i < count * numberOfChannels; i++ ) {
-        dst[i] /= value;
-    }
-}
-
-template<typename T1, typename T2, typename T3>
-void pixels_divide( T1* dst, const T2* src, T3 value, size_t count, int numberOfChannels = 1 )
-{
-    for( size_t i = 0; i < count * numberOfChannels; i++ ) {
-        dst[i] = src[i] / value;
-    }
+    return pixels_histogram_p( h, channel, h.PixelsCount() / 2 );
 }
 
 #endif // IMAGE_MATH_H
